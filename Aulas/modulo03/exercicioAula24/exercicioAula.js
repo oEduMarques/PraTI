0dd8dca5ba16987267dab7c34976557e320ab25d
oEduMarques <eduardomesser321@gmail.com>
2024-09-14 18:02:23 -0300

    Modelos de formualrios

---
+++ Aulas/modulo03/exercicioAula24/exercicioAula.js
@@ -0,0 +1,26 @@
+// Formulário de contato com validação
+//     Objetivo: Criar um formulário de contato, com validação.
+//     Instruções:
+//         Crie um formulário com os seguintes campos:
+//             nome(obrigatorio)
+//             email (obrigatorio e deve validar formato de email)
+//             mensagem(obrigatório)
+//             telefone(opcional)
+//             assunto (campo de seleção com 3 opções: Dúvida, Suporte, Feedback)
+//             Checkbox para aceitar os temos de uso (obrigatório)
+
+//         inclua um botão de envio que só deve ser habilitado se todos os campo obrigatórios forem preenchidos corretamente.
+//         Adicione um placeholder para cada campo
+
+//         Utilize HTML5 para adicionar validação dos campos obrigatórios e padrões de vlidação (como o campo de email e número de telefone).
+
+// CSS - Estilização e responsividade:
+
+//     Estilização 
+
+//     Adicione ua borda ao redor do formulário com cantos arredondados.
+//     Utilize espaçamento adequado entre os campos.
+//     Altere a cor de fundo e o estilo da fonte.
+//     Aplique foco (:focus) nos campos, destacando-os quando o usuário estiver interagindo com else.
+//     Estilize o botão de envio para parecer um botão moderno(com sombras, efeito hover e animação quando pressionado).
+//     Alinhe os rótulos (<label></label>) dos campos à esquerda e os inputs a DeviceOrientationEvent, criando uma boa legibilidade
