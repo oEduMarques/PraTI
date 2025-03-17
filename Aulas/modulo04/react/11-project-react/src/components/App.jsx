427ba7bbc37101c9235ad387aa8f76be21877ce7
Eduardo Messer Marques <eduardo.marques@hmv.org.br>
2025-03-17 09:39:07 -0300

    Adicionado o modulo react

---
+++ Aulas/modulo04/react/11-project-react/src/components/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Navigation } from './Navigation'
+import { Content } from './Content'
+
+function App() {
+
+  return (
+    <>
+      <Navigation/>
+      <Content />
+    </>
+  )
+}
+
+export default App
